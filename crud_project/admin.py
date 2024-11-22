c855a82c07ef728b627df9a8be7a54720e3b37e5
chaimaaloui <alouichaima.77@gmail.com>
2024-11-22 11:58:23 +0100

    commit

--- crud_project/admin.py
+++ crud_project/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from crud_project.models import Author, BlogPost
 
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):  
+    list_display = ("title", "content", "created_on", "author")  
+    list_editable = ("content",) 
+    list_display_links = ("title",)  
+    search_fields = ("title", "content")  
+    list_filter = ("created_on",)  
+    list_per_page = 10  
